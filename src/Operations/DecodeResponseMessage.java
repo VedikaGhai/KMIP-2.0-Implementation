fc102d59b45ae85dd836862499e403ba5e15ee6b
SohaWithAnS <soha141198@gmail.com>
2020-01-06 10:59:21 +0530

    Decode Response Message

---
+++ src/Operations/DecodeResponseMessage.java
@@ -0,0 +1,42 @@
+package Operations;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
+
+public class DecodeResponseMessage {
+
+    public void DOMParser() throws SAXException, IOException, ParserConfigurationException
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.parse(new File("/home/soha/Documents/Response1.xml"));
+
+        document.getDocumentElement().normalize();
+
+        Element root = document.getDocumentElement();
+        //System.out.println(root.getNodeName());
+
+        NodeList nList = document.getElementsByTagName("KeyMaterial");
+        System.out.println("============================");
+        System.out.println(nList.getLength());
+        for(int temp=0; temp< nList.getLength(); temp++)
+        {
+            Node node = nList.item(temp);
+            Element e= (Element)node;
+            System.out.println(e.getAttribute("value"));
+
+            
+        }
+
+    }
+
+    public static void main(String args[]) throws SAXException, IOException, ParserConfigurationException
+    {
+        DecodeResponseMessage d = new DecodeResponseMessage();
+        d.DOMParser();
+    }
+
+}
