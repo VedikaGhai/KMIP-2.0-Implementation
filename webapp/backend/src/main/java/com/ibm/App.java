6631ec8c422404317e0e9cca2e0da9df3c8849ce
VedikaGhei <41205506+VedikaGhei@users.noreply.github.com>
2020-05-16 15:24:31 +0530

    Upload webapp-backend

---
+++ webapp/backend/src/main/java/com/ibm/App.java
@@ -0,0 +1,18 @@
+package com.ibm;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+       // System.out.println( "Hello World!" );
+       
+    }
+}
---
+++ webapp/backend/src/test/java/com/ibm/AppTest.java
@@ -0,0 +1,14 @@
+package com.ibm;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+   
+}
